683228011cb0982a915e5868496b98110a297e64
unitylearner <unitylearner@163.com>
2015-11-24 22:26:19 +0800

    Array2D.1.1

--- Array2D.h
+++ Array2D.h
@@ -1,7 +1,26 @@
 #ifndef ARRAY2D_H_
 #define ARRAY2D_H_
 #include "Array.h"
-#include<stdexcept>
+
+template<class T> class Array2D;
+template<class T> class Row;
+
+template<class T>
+class Row
+{
+public:
+	Array2D<T>& array2D;
+	unsigned int const row;
+public:
+	Row(Array2D<T>& _array2D, unsigned int _row) :
+		array2D(_array2D), row(_row) {}
+
+	T & operator[](unsigned int column) const
+	{
+		return array2D.Select(row, column);
+	}
+};
+
 template<class T>
 class Array2D
 {
@@ -10,35 +29,24 @@ protected:
 	unsigned int numberOfColumns;
 	Array<T> array;
 public:
-	class Row<T>
-	{
-		Array2D<T>& array2D;
-		unsigned int const row;
-	public:
-		Row<T>(Array2D<T>& _array2D, unsigned int _row) :
-			array2D(_array2D), row(_row){}
-		T & operator[](unsigned int column) const
-		{
-			return array2D.Select(row, column);
-		}
-	};
 
 	Array2D(unsigned int, unsigned int);
 	T & Select(unsigned int, unsigned int);
-	Row<T> operator[](unsigned int row);
+	Row<T>& operator [] (unsigned int row);
 };
 
 template<class T>
 Array2D<T>::Array2D(unsigned int m, unsigned int n) :
-numberOfRows(m),
-numberOfColumns(n),
-array(m * n)
+	numberOfRows(m),
+	numberOfColumns(n),
+	array(m * n)
 {}
 
 template<class T>
-Array2D<T>::Row<T> Array2D<T>::operator[](unsigned int row)
+Row<T>& Array2D<T>::operator [] (unsigned int row)
 {
-	return Row<T>(*this, row);
+	Row<T>* temp = new Row<T>(*this, row);
+	return *temp;
 }
 
 template<class T>
@@ -50,6 +58,5 @@ T & Array2D<T>::Select(unsigned int i, unsigned int j)
 		throw std::out_of_range("invalid column");
 	return array[i * numberOfColumns + j];
 }
-
 
 #endif
